64972493e9824e5878c326c34e6820e7ad353d19
Lucas Magno <lucas.mv.silva@gmail.com>
2019-06-12 10:15:56 -0300

    Areas

---
+++ AppModelo/src/Dev.Ui.Site/Modulos/Vendas/Controllers/PedidosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dev.Ui.Site.Modulos.Vendas.Controllers
+{
+    [Area("Vendas")]
+    public class PedidosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- AppModelo/src/Dev.Ui.Site/Startup.cs
+++ AppModelo/src/Dev.Ui.Site/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Dev.Ui.Site {
@@ -13,6 +14,14 @@ namespace Dev.Ui.Site {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+
+            services.Configure<RazorViewEngineOptions>(options => {
+                options.AreaViewLocationFormats.Clear();
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -26,6 +35,10 @@ namespace Dev.Ui.Site {
 
             app.UseMvc(routes => {
                 routes.MapRoute("default", "{controller=Home}/{Action=Index}/{id?}");
+
+                //routes.MapRoute("areas", "{area=exists}/{controller=Home}/{Action=Index}/{id?}");
+                routes.MapAreaRoute(name: "AreaProdutos", areaName: "Produtos", template: "Produtos/{controller=Cadastro}/{Action=Index}/{id?}");
+                routes.MapAreaRoute(name: "AreaVendas", areaName: "Vendas", template: "Vendas/{controller=Pedidos}/{Action=Index}/{id?}");
             });
         }
     }
