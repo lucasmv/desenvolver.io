3fcdb09f1f10205f6599eeef061ebb7548ecef73
Lucas Veiga <lucas.mv.silva@gmail.com>
2020-09-16 08:13:22 -0300

    Introdução

---
+++ .NET/MinhaPrimeiraAPI/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MinhaPrimeiraAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+
+            return new string[] { "value1", "value2", "value3" };
+        }
+    }
+}
